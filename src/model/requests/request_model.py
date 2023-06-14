dd2da3e7b3b677bad16f9168d8b90a1231726ee9
thomas <thomsar37oss@gmail.com>
2023-06-14 17:47:06 -0400

    feature(#32): updated browser validation module

--- src/model/requests/request_model.py
+++ src/model/requests/request_model.py
@@ -12,7 +12,14 @@ class ClientInfo:
         self.device_type = device_type
 
     def is_browser(self) -> bool:
-        return self.browser != "Other"
+        if (
+            self.browser == "Chrome"
+            or self.browser == "Firefox"
+            or self.browser == "Safari"
+            or self.browser == "Edge"
+        ):
+            return True
+        return False
 
 
 def parse_user_agent(user_agent: str) -> Optional[ClientInfo]:
