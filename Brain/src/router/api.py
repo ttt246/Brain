1dabf5e703d73c9bb6b42988c7a96ef9732ac14b
hidekisato927 <hideki.ios.dev@gmail.com>
2023-07-03 17:11:20 -0700

    feature(#134): make APIs for storing contacts into firestore

--- Brain/src/router/api.py
+++ Brain/src/router/api.py
@@ -61,7 +61,7 @@ def construct_blueprint_api() -> APIRouter:
 
     @router.post("/sendNotification")
     def send_notification(
-        data: Notification, client_info: ClientInfo = Depends(get_client_info)
+            data: Notification, client_info: ClientInfo = Depends(get_client_info)
     ):
         # firebase admin init
         try:
@@ -90,7 +90,7 @@ def construct_blueprint_api() -> APIRouter:
 
         # check contact querying
         try:
-            contacts_service = ContactsService(setting=setting)
+            contacts_service = ContactsService(firebase_app=firebase_app, setting=setting)
             if result["program"] == ProgramType.AUTO_TASK:
                 auto_task_service = AutoTaskService()
                 result["content"] = auto_task_service.ask_task_with_autogpt(
@@ -194,7 +194,7 @@ def construct_blueprint_api() -> APIRouter:
         try:
             # check about asking image description with trained data
             if query_image_ask(
-                image_content=image_content, message=message, setting=setting
+                    image_content=image_content, message=message, setting=setting
             ):
                 image_response["image_desc"] = image_content
             else:
@@ -413,7 +413,7 @@ def construct_blueprint_api() -> APIRouter:
             for contact in data.contacts:
                 contacts.append(assembler.to_contact_model(contact))
             # train contact
-            contacts_service = ContactsService(setting=setting)
+            contacts_service = ContactsService(firebase_app=firebase_app, setting=setting)
             contacts_service.train(uuid, contacts)
         except Exception as e:
             if isinstance(e, BrainException):
@@ -444,7 +444,7 @@ def construct_blueprint_api() -> APIRouter:
 
             # parsing contacts
             # train contact
-            contacts_service = ContactsService(setting=setting)
+            contacts_service = ContactsService(firebase_app=firebase_app, setting=setting)
             contacts_service.delete_all(uuid)
         except Exception as e:
             if isinstance(e, BrainException):
@@ -490,6 +490,35 @@ def construct_blueprint_api() -> APIRouter:
             return assembler.to_response(400, "Failed to delete data", "")
         return assembler.to_response(
             200, "Deleted data from real-time database of firebase", ""
+        )
+
+    """@generator.request_body(
+            {
+                "token": "String",
+                "uuid": "String", 
+            }
+        )
+
+        @generator.response(
+            status_code=200, schema={"message": "message", "result": "test_result"}
+        )
+
+        """
+
+    @router.post("/contacts/getByUUID")
+    def getContactsByUUID(data: BasicReq):
+        try:
+            setting, firebase_app = firebase_admin_with_setting(data)
+        except BrainException as ex:
+            return ex.get_response_exp()
+
+        token: str = setting.token
+        uuid: str = setting.uuid
+
+        result = ContactsService(firebase_app=firebase_app, setting=setting).getContactsByUUID(uuid=uuid)
+
+        return assembler.to_response(
+            200, "Success to get contacts by uuid", result
         )
 
     return router
--- Brain/src/service/contact_service.py
+++ Brain/src/service/contact_service.py
@@ -1,6 +1,8 @@
 """service to manage contacts"""
 from typing import List, Any
 
+import google
+
 from Brain.src.model.req_model import ReqModel
 from Brain.src.rising_plugin.csv_embed import get_embed
 from Brain.src.rising_plugin.pinecone_engine import (
@@ -15,10 +17,18 @@ from Brain.src.rising_plugin.pinecone_engine import (
 from Brain.src.common.utils import PINECONE_INDEX_NAME
 from Brain.src.model.contact_model import ContactModel, ContactStatus
 
+import firebase_admin
+from firebase_admin import firestore
+
 
 class ContactsService:
-    def __init__(self, setting: ReqModel):
+    db: Any
+    phones_ref: Any
+
+    def __init__(self, firebase_app: firebase_admin.App, setting: ReqModel):
+        self.firebase_app = firebase_app
         self.setting = setting
+        self.init_firestore()
 
     """train contacts (getting embedding) and update pinecone with embeddings by contact_id
     train datatype:
@@ -28,7 +38,9 @@ class ContactsService:
     def train(self, uuid: str, contacts: List[ContactModel]) -> None:
         # getting index namespaceof pinecone
         pinecone_namespace = self.get_pinecone_index_namespace(uuid)
+
         for contact in contacts:
+            # Save the data
             # generate key and values for pinecone data
             key = contact.contact_id
             value = f"{contact.display_name}, {contact.get_str_phones()}"
@@ -42,12 +54,14 @@ class ContactsService:
                     value=vectoring_values,
                     setting=self.setting,
                 )
+                self.create_one_contact(uuid=uuid, contact=contact)
             elif contact.status == ContactStatus.DELETED:
                 delete_pinecone(
                     namespace=pinecone_namespace,
                     key=key,
                     setting=self.setting,
                 )
+                self.delete_one_contact(uuid=uuid, contact=contact)
             elif contact.status == ContactStatus.UPDATED:
                 update_pinecone(
                     namespace=pinecone_namespace,
@@ -55,6 +69,7 @@ class ContactsService:
                     value=vectoring_values,
                     setting=self.setting,
                 )
+                self.update_one_contact(uuid=uuid, contact=contact)
 
     """"query contact with search text
     response: list of contactId as index key of pinecone"""
@@ -85,3 +100,50 @@ class ContactsService:
 
     def get_pinecone_index_namespace(self, uuid: str) -> str:
         return get_pinecone_index_namespace(f"{uuid}-contacts")
+
+    """init firestore to save user's contacts"""
+    def init_firestore(self):
+        self.db = firestore.client(app=self.firebase_app)
+        self.phones_ref = self.db.collection("phones")
+
+    """create a contact into document which name is uuid in phone collections in firestore"""
+    def create_one_contact(self, uuid: str, contact: ContactModel):
+        data = {
+            "contactId": contact.contact_id,
+            "displayName": contact.display_name,
+            "phoneNumbers": contact.phone_numbers
+        }
+        phones_doc_ref = self.phones_ref.document(uuid)
+        contacts_doc_ref = phones_doc_ref.collection("contacts").document(contact.contact_id)
+        contacts_doc_ref.set(data)
+
+    """update a contact into document which name is uuid in phone collections in firestore"""
+    def update_one_contact(self, uuid: str, contact: ContactModel):
+        data = {
+            "contactId": contact.contact_id,
+            "displayName": contact.display_name,
+            "phoneNumbers": contact.phone_numbers
+        }
+        phones_doc_ref = self.phones_ref.document(uuid)
+        contacts_doc_ref = phones_doc_ref.collection("contacts").document(contact.contact_id)
+        contacts_doc_ref.update(data)
+
+    """delete a contact into document which name is uuid in phone collections in firestore"""
+    def delete_one_contact(self, uuid: str, contact: ContactModel):
+        phones_doc_ref = self.phones_ref.document(uuid)
+        contacts_doc_ref = phones_doc_ref.collection("contacts").document(contact.contact_id)
+        contacts_doc_ref.delete()
+
+    def getContactsByUUID(self, uuid: str) -> []:
+        phones_doc_ref = self.phones_ref.document(uuid)
+        contacts_ref = phones_doc_ref.collection("contacts")
+
+        # Retrieve all documents in the 'contacts' sub-collection
+        contacts = contacts_ref.stream()
+
+        result = []
+        # Iterate through the documents and print out their data
+        for contact in contacts:
+            result.append(contact.to_dict())
+
+        return result
