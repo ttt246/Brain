689c7aea41b37498cfbd2f554cd9615f09595198
peterluo3131 <peterluo3131@gmail.com>
2023-07-14 11:17:13 -0400

    feature: remove unused log module

--- Android/app/src/main/java/com/matthaigh27/chatgptwrapper/ui/setting/view/SettingActivity.kt
+++ Android/app/src/main/java/com/matthaigh27/chatgptwrapper/ui/setting/view/SettingActivity.kt
@@ -2,7 +2,6 @@ package com.matthaigh27.chatgptwrapper.ui.setting.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
